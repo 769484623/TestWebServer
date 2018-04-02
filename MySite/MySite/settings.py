3a4ca100be5fea2928e728d2b7e195d180ea4c47
769484623 <769484623@qq.com>
2018-04-02 14:49:10 +0000

    Add MainFrame,use mysql instead of sqlite. change timezone

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MySite',
+        'USER': 'django',
+        'PASSWORD': 'djangothewebsite',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
