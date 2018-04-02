4e94e1ff06d2dbe8c6da46a212aadaa8aeb8ab5f
LNStar <769484623@qq.com>
2018-04-02 20:01:47 +0800

    Add Gunicorn & make a script to reduce the work of typing

---
+++ MySite/StartServer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# > 1 && $1 == "DEBUG" ];then
+    echo "Developing Mode."
+    ./manage.py runserver 0.0.0.0:80
+else
+    echo "Server is Online."
+    gunicorn -w 3 -b 0.0.0.0:80  MySite.wsgi:application
+fi
